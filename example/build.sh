77a11b45116b39e6db41ff71d8dd7fd63aad6411
Neil <neil.deng.g@gmail.com>
2022-03-31 11:54:26 +0800

    add example shell

---
+++ example/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash +x
+set -e
+
+# change env to you wanted
+IMAGE_TAIL_CODE="customized"
+GRAFANA_VERSION=8.4.2  # TESTED VERSIONS: 8.4.1、8.4.2、8.4.4
+FAVICON="https://web-eshop.cdn.hinet.net/eshop/img/favicon.ico"
+LOGO_ICON="https://play.grafana.org/public/img/grafana_icon.svg"
+APP_TITLE="Grafana"
+LOGIN_TITLE="Welcom to Grafana"
+GET_LOGIN_SUB_TITLE="This is sub title"
+
+# build customize image with env
+docker build -t grafana/grafana:$GRAFANA_VERSION-$IMAGE_TAIL_CODE . \
+  --build-arg GRAFANA_VERSION=$GRAFANA_VERSION \
+  --build-arg FAVICON="$FAVICON" \
+  --build-arg LOGO_ICON="$LOGO_ICON" \
+  --build-arg APP_TITLE="$APP_TITLE" \
+  --build-arg LOGIN_TITLE="$LOGIN_TITLE" \
+  --build-arg GET_LOGIN_SUB_TITLE="$GET_LOGIN_SUB_TITLE"
+  
